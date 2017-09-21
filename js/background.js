45a010cf70823cd1b3cadde373e5afacfcda9b60
tyleryep <tyep@stanford.edu>
2017-09-20 21:05:58 -0700

    Fix CDNs, code formatting

--- js/background.js
+++ js/background.js
@@ -41,23 +41,19 @@ function getTime() {
 }
 
 function changeBackground(button) {
-    var homePage = document.getElementById("sky");
+    var homePage = document.getElementById("home");
     var header = document.getElementById("splash");
     if (button == "sunrise") {
-        // homePage.style.backgroundImage = "url('img/sunrise.jpg')";
-        homePage.src = "img/sunrise.jpg";
+        homePage.style.backgroundImage = "url('img/sunrise.jpg')";
         header.style.color = "inherit";
     } else if (button == "sun") {
-        // homePage.style.backgroundImage = "url('img/sky.jpg')";
-        homePage.src = "img/sky.jpg";
+        homePage.style.backgroundImage = "url('img/sky.jpg')";
         header.style.color = "inherit";
     } else if (button == "sunset") {
-        // homePage.style.backgroundImage = "url('img/sunset.jpg')";
-        homePage.src = "img/sunset.jpg";
+        homePage.style.backgroundImage = "url('img/sunset.jpg')";
         header.style.color = "inherit";
     } else if (button == "night") {
-        // homePage.style.backgroundImage = "url('img/nightsky.jpg')";
-        homePage.src = "img/nightsky.jpg";
+        homePage.style.backgroundImage = "url('img/nightsky.jpg')";
         header.style.color = "white";
     }
 }
