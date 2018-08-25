8336f2fd8ea3f67e369c4587574e622861f88e86
Tyler Yep <tyep@corp.intuit.net>
2018-08-24 21:09:48 -0700

    smooth scrolling, 2-space indentation

--- js/background.js
+++ js/background.js
@@ -1,81 +1,91 @@
 getTime();
 
-document.getElementById("sunrisebutton").addEventListener("click", function() {
-    changeBackground("sunrise");
-    removeMoon();
-    sunriseSunset();
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({behavior: 'smooth'});
+  });
 });
-document.getElementById("sunbutton").addEventListener("click", function() {
-    changeBackground("sun");
-    removeMoon();
-    bringSun();
+
+document.getElementById("sunrisebutton").addEventListener("click", () => {
+  changeBackground("sunrise");
+  removeMoon();
+  sunriseSunset();
 });
-document.getElementById("sunsetbutton").addEventListener("click", function() {
-    changeBackground("sunset");
-    removeMoon();
-    sunriseSunset();
+
+document.getElementById("sunbutton").addEventListener("click", () => {
+  changeBackground("sun");
+  removeMoon();
+  bringSun();
 });
-document.getElementById("nightbutton").addEventListener("click", function() {
-    changeBackground("night");
-    removeSun();
-    bringMoon();
+
+document.getElementById("sunsetbutton").addEventListener("click", () => {
+  changeBackground("sunset");
+  removeMoon();
+  sunriseSunset();
+});
+
+document.getElementById("nightbutton").addEventListener("click", () => {
+  changeBackground("night");
+  removeSun();
+  bringMoon();
 });
 
 function getTime() {
-    let currentTime = new Date().getHours();
-    if (currentTime >= 0 && currentTime < 5 || currentTime >= 19) {
-        changeBackground("night");
-        bringMoon();
-    } else if (currentTime >= 5 && currentTime < 8) {
-        changeBackground("sunrise");
-        sunriseSunset();
-    } else if (currentTime >= 16 && currentTime < 19) {
-        changeBackground("sunset");
-        sunriseSunset();
-    } else {
-        bringSun();
-    }
+  const currentTime = new Date().getHours();
+  if (currentTime >= 0 && currentTime < 5 || currentTime >= 19) {
+    changeBackground("night");
+    bringMoon();
+  } else if (currentTime >= 5 && currentTime < 8) {
+    changeBackground("sunrise");
+    sunriseSunset();
+  } else if (currentTime >= 16 && currentTime < 19) {
+    changeBackground("sunset");
+    sunriseSunset();
+  } else {
+    bringSun();
+  }
 }
 
 function changeBackground(button) {
-    let homePage = document.getElementById("home");
-    let header = document.getElementById("splash");
-    if (button == "sunrise") {
-        homePage.style.backgroundImage = "url('img/home/sunrise.jpg')";
-        header.style.color = "inherit";
-    } else if (button == "sun") {
-        homePage.style.backgroundImage = "url('img/home/sky.jpg')";
-        header.style.color = "inherit";
-    } else if (button == "sunset") {
-        homePage.style.backgroundImage = "url('img/home/sunset.jpg')";
-        header.style.color = "inherit";
-    } else if (button == "night") {
-        homePage.style.backgroundImage = "url('img/home/nightsky.jpg')";
-        header.style.color = "white";
-    }
+  let homePage = document.getElementById("home");
+  let header = document.getElementById("splash");
+  if (button == "sunrise") {
+    homePage.style.backgroundImage = "url('img/home/sunrise.jpg')";
+    header.style.color = "inherit";
+  } else if (button == "sun") {
+    homePage.style.backgroundImage = "url('img/home/sky.jpg')";
+    header.style.color = "inherit";
+  } else if (button == "sunset") {
+    homePage.style.backgroundImage = "url('img/home/sunset.jpg')";
+    header.style.color = "inherit";
+  } else if (button == "night") {
+    homePage.style.backgroundImage = "url('img/home/nightsky.jpg')";
+    header.style.color = "white";
+  }
 }
 
 function removeSun() {
-    let sun = document.getElementById("sun");
-    sun.style.top = "70%";
+  let sun = document.getElementById("sun");
+  sun.style.top = "70%";
 }
 
 function bringSun() {
-    let sun = document.getElementById("sun");
-    sun.style.top = "30%";
+  let sun = document.getElementById("sun");
+  sun.style.top = "30%";
 }
 
 function removeMoon() {
-    let moon = document.getElementById("moon");
-    moon.style.top = "70%";
+  let moon = document.getElementById("moon");
+  moon.style.top = "70%";
 }
 
 function bringMoon() {
-    let moon = document.getElementById("moon");
-    moon.style.top = "30%";
+  let moon = document.getElementById("moon");
+  moon.style.top = "30%";
 }
 
 function sunriseSunset() {
-    let sun = document.getElementById("sun");
-    sun.style.top = "60%";
+  let sun = document.getElementById("sun");
+  sun.style.top = "60%";
 }
