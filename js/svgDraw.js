c7f658b9319cbdf6c17bac274152e3781221e949
Tyler Yep <tyep@corp.intuit.net>
2018-08-29 00:03:39 -0700

    programmatically adds svg-canvas

--- js/svgDraw.js
+++ js/svgDraw.js
@@ -1,11 +1,32 @@
+function connectAllDivs() {
+  connectDivs("a", "b", "#2cd6c2", 0.6);
+  connectDivs("c", "d", "#2cd6c2", 0.6);
+  connectDivs("d", "a", "#2cd6c2", 0.6);
+  connectDivs("e", "f", "#2cd6c2", 0.6);
+
+}
+
+
+window.onresize = event => {
+  const svg = document.getElementById("svg-canvas");
+  while (svg.firstChild) {
+    svg.removeChild(svg.firstChild);
+  }
+  connectAllDivs();
+};
+
+connectAllDivs();
+
+
+/* --- Helper functions --- */
+
 function createSVG() {
   let svg = document.getElementById("svg-canvas");
-  if (null == svg) {
+  if (svg == null) {
     svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
     svg.setAttribute('id', 'svg-canvas');
-    svg.setAttribute('style', 'position:absolute; height: 100%; width: 100%; z-index: 5;');
 
-    let p = document.getElementById("project-slide");
+    let p = document.getElementsByClassName("carousel-item active")[0];
     p.insertBefore(svg, p.firstChild);
   }
   return svg;
@@ -20,30 +41,16 @@ function findAbsolutePosition(htmlElement) {
   return {"x": x, "y": y};
 }
 
-function connectDivs(leftId, rightId, color, tension) {
-  const left = document.getElementById(leftId);
-  const right = document.getElementById(rightId);
-
-  const leftPos = findAbsolutePosition(left);
-  let x1 = leftPos.x;
-  let y1 = leftPos.y - 3 * document.body.clientHeight;
-
-  const rightPos = findAbsolutePosition(right);
-  let x2 = rightPos.x;
-  let y2 = rightPos.y - 3 * document.body.clientHeight;
-
-  drawCurvedLine(x1, y1, x2, y2, color, tension);
-}
-
 function drawCurvedLine(x1, y1, x2, y2, color, tension) {
   const svg = createSVG();
-  let shape = document.createElementNS("http://www.w3.org/2000/svg", "path");
-  let delta = (x2 - x1) * tension;
+  const delta = (x2 - x1) * tension;
   let hx1 = x1 + delta;
   let hy1 = y1;
   let hx2 = x2 - delta;
   let hy2 = y2;
-  let path = "M " + x1 + " " + y1 + " C " + hx1 + " " + hy1 + " " + hx2 + " " + hy2 + " " + x2 + " " + y2;
+  const path = "M " + x1 + " " + y1 + " C " + hx1 + " " + hy1 + " " + hx2 + " " + hy2 + " " + x2 + " " + y2;
+
+  let shape = document.createElementNS("http://www.w3.org/2000/svg", "path");
   shape.setAttribute("d", path);
   shape.setAttribute("stroke", color);
   shape.setAttribute("style", "fill: none;");
@@ -51,12 +58,17 @@ function drawCurvedLine(x1, y1, x2, y2, color, tension) {
   svg.appendChild(shape);
 }
 
-// window.onresize = event => {
-//   const svg = document.getElementById("svg-canvas");
-//   while (svg.firstChild) {
-//     svg.removeChild(svg.firstChild);
-//   }
-//   connectDivs("a", "b", "#2cd6c2", 0.6);
-// };
-//
-// connectDivs("a", "b", "#2cd6c2", 0.6);
+function connectDivs(leftId, rightId, color, tension) {
+  const left = document.getElementById(leftId);
+  const right = document.getElementById(rightId);
+
+  const leftPos = findAbsolutePosition(left);
+  const x1 = leftPos.x;
+  const y1 = leftPos.y - 3 * document.body.clientHeight; // Projects page is 3rd from the top
+
+  const rightPos = findAbsolutePosition(right);
+  const x2 = rightPos.x;
+  const y2 = rightPos.y - 3 * document.body.clientHeight; // Projects page is 3rd from the top
+
+  drawCurvedLine(x1, y1, x2, y2, color, tension);
+}
