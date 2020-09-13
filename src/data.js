d892609cec23f959fe86101fa551b1ea3dfef647
Tyler Yep <tyep@cs.stanford.edu>
2020-09-12 20:25:18 -0700

    bubbles displaying correctly using data.js

---
+++ src/data.js
@@ -0,0 +1,54 @@
+export const carouselData = [
+    {
+      year: 2017,
+      lanes: [
+        [
+          {
+            index: 1,
+            image: 'edutech.jpg',
+            overlay: 'red',
+            text: 'EduTech',
+          },
+        ],
+        [
+          {
+            index: 0,
+            image: 'stanford.jpg',
+            overlay: 'red',
+            text: 'Stanford',
+          },
+          {
+            index: 1,
+            image: 'marker',
+            text: 'Winter Quarter',
+          },
+          {
+            index: 2,
+            image: 'marker',
+            text: 'Spring Quarter',
+          },
+          {
+            index: 3,
+            image: 'openproof.jpg',
+            overlay: 'red',
+            text: 'Stanford',
+          },
+        ],
+        [],
+        [
+          {
+            index: 2,
+            image: 'cs198.png',
+            overlay: 'red',
+            text: 'CS 198',
+          },
+          {
+            index: 3,
+            image: 'marker',
+            text: 'Summer Quarter',
+          },
+        ],
+      ],
+      connections: {},
+    },
+  ]
--- src/index.js
+++ src/index.js
@@ -1,54 +1,71 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import * as serviceWorker from "./serviceWorker";
-import Carousel from "react-bootstrap/Carousel";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import * as serviceWorker from './serviceWorker'
+import Carousel from 'react-bootstrap/Carousel'
 
-function Bubble() {
+import { carouselData } from './data'
+
+const numMap = ['zero', 'one', 'two', 'three', 'four', 'five']
+
+function Marker(props) {
   return (
-    <div id="stanford" className="bubble one">
-      <img src="img/projects/stanford.jpg" alt="Stanford" />
-      <span className="overlay red">
-        <p className="text">Stanford</p>
+    <div
+      id={`marker-${props.index}`}
+      className={`marker ${numMap[props.index]}`}
+      data-toggle="tooltip"
+      data-placement="top"
+      title={props.text}
+    ></div>
+  )
+}
+
+function Bubble(props) {
+  return (
+    <div id="stanford" className={`bubble ${numMap[props.index]}`}>
+      <img src={`img/projects/${props.image}`} alt={props.text} />
+      <span className={`overlay ${props.color}`}>
+      <p className="text">{props.text}</p>
       </span>
     </div>
-  );
+  )
 }
 
 function Lane(props) {
   return (
-    <div className={props.top ? "lane top" : "lane"}>
+    <div className={props.top ? 'lane top' : 'lane'}>
       {props.bubbles.map((bubble) => (
-        <Bubble key={bubble} />
+        (bubble.image === 'marker')
+        ? <Marker key={bubble.index} {...bubble} />
+        : <Bubble key={bubble.index} {...bubble} />
       ))}
     </div>
-  );
+  )
 }
 
 function ProjectCarousel() {
   return (
     <Carousel id="project" interval={null}>
-      {[0, 1, 2].map((num) => (
-        <Carousel.Item key={num}>
+      {carouselData.map((slide) => (
+        <Carousel.Item key={slide.year}>
           <h1>Projects</h1>
           <img src="img/projects/stars.jpg" alt="stars"></img>
           <Carousel.Caption>
-            <h2 className="year">201{num}</h2>
-            <Lane top bubbles={[1, 2]} />
-            <Lane bubbles={[3]} />
-            <Lane bubbles={[]} />
-            <Lane bubbles={[]} />
+            <h2 className="year">{slide.year}</h2>
+            {slide.lanes.map((lane, i) => (
+              <Lane key={i} top={i === 0} bubbles={lane} />
+            ))}
           </Carousel.Caption>
         </Carousel.Item>
       ))}
     </Carousel>
-  );
+  )
 }
 
-// TODO: Add <React.StrictMode> here when react bootstrap is
-ReactDOM.render(<ProjectCarousel />, document.querySelector("#projects"));
+// TODO: Add <React.StrictMode> here when react bootstrap is ready.
+ReactDOM.render(<ProjectCarousel />, document.querySelector('#projects'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
 // TODO: re-enable cross origin checks in Safari
