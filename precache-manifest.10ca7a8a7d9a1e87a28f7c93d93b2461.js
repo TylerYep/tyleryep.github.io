b978020494d7624aca80156f31f02dbd30676d30
Tyler Yep <tyep@cs.stanford.edu>
2020-09-15 11:23:43 -0700

    Updates

---
+++ precache-manifest.10ca7a8a7d9a1e87a28f7c93d93b2461.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ba6f15e2d58bfc34ba589623dde598d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "001412d1d09f8a08732c",
+    "url": "/static/js/2.a31cf662.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.a31cf662.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84a501f5f718c2f2c436",
+    "url": "/static/js/main.cf29f41d.chunk.js"
+  },
+  {
+    "revision": "836bd14502054e16a2bc",
+    "url": "/static/js/runtime-main.583f7dbc.js"
+  }
+]);
