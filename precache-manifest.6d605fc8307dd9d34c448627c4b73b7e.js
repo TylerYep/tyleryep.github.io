fcb6869ccfae32f15675f72cc7d8551410b0b378
Tyler Yep <tyep@cs.stanford.edu>
2021-01-21 21:59:44 -0800

    Updates

--- precache-manifest.6d605fc8307dd9d34c448627c4b73b7e.js
+++ precache-manifest.6d605fc8307dd9d34c448627c4b73b7e.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "7b436d2e6a441e983709f5cd637af208",
+    "revision": "dd480c8da2ec11b6775b63bc90bc7aac",
     "url": "/index.html"
   },
   {
-    "revision": "97ee7066ac4fb16a39f7",
-    "url": "/static/js/2.fc5f816f.chunk.js"
+    "revision": "d91edb092b0bfb561f37",
+    "url": "/static/js/2.04deb29c.chunk.js"
   },
   {
     "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
-    "url": "/static/js/2.fc5f816f.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.04deb29c.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "19ae24b63420f2963566",
-    "url": "/static/js/main.6f7d0415.chunk.js"
+    "revision": "5f62fb0625877f15d0e9",
+    "url": "/static/js/main.0b51d37d.chunk.js"
   },
   {
     "revision": "836bd14502054e16a2bc",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.36ef66e4c775f356ea4202056c0fa7a7.js"
+  "/precache-manifest.6d605fc8307dd9d34c448627c4b73b7e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
